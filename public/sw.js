2810a4f46390e1744d4e48dd9b3a9647ccf785bc
BenWassa <haddonbenjamin@gmail.com>
2026-03-22 14:50:19 -0400

    release: v3.13.4

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // Rewritten by scripts/versioning.js on each build.
-const BUILD_VERSION = '3.13.3';
-const BUILD_ID = '3.13.3-20260322183643-0f027b7';
+const BUILD_VERSION = '3.13.4';
+const BUILD_ID = '3.13.4-20260322185008-f95d8d0';
 const CACHE_NAME = `hearth-static-${BUILD_ID.replace(/[^a-zA-Z0-9-]/g, '-')}`;
 const OFFLINE_SHELL = '/index.html';
 
